05d00978a94a0c05cbea0f5b4a1f536cb650ae1f
RobertKlivis <roberts.klivis@gmail.com>
2018-10-20 23:54:57 +0100

    Added collision angle
    Works better with the current speed but still with issues.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -9,16 +9,19 @@ public class Movement : MonoBehaviour {
     public float mass;
     public GameObject car;
 
+    public float collisionAngle;
+
 
     // Use this for initialization
     void Start () {
 
         acceleration = 9.8f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        
         float currentVelocity = (velocity / mass) * acceleration;
 
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -29,6 +32,22 @@ public class Movement : MonoBehaviour {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.right), currentVelocity * Time.deltaTime);
+        }
+
+
+    }
+
+   void OnCollisionEnter(Collision collision)
+    {
+
+        float currentVelocity = (velocity / mass) * acceleration;
+
+        Vector2 normal = collision.contacts[0].normal;
+        collisionAngle = 90 - Vector2.Angle(transform.position, -normal);
+
+        if (collisionAngle > 1.7)
+        {
+            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.up), currentVelocity * Time.deltaTime);
         }
 
 
