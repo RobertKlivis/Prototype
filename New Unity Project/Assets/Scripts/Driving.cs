229c8b44a0e78489ca50d957273c76dc96a5c1e7
cbevan200 <CBEVAN200@caledonian.ac.uk>
2018-10-23 10:25:37 +0100

    Build change
    Using build with 2D car and background

---
+++ New Unity Project/Assets/Scripts/Driving.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving : MonoBehaviour {
+
+    public float Velocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.left), Velocity * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.right), Velocity * Time.deltaTime);
+        }
+
+    }
+}
