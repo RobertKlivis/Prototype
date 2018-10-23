4a9efcfa9bd29c68df7b107dd6b746efd2240b4e
cbevan200 <CBEVAN200@caledonian.ac.uk>
2018-10-23 11:59:49 +0100

    Gravity implementation
    Gravity has been implemented

--- New Unity Project/Assets/Scripts/Driving.cs
+++ New Unity Project/Assets/Scripts/Driving.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class Driving : MonoBehaviour {
 
-    public float Velocity;
+    public float velocity;
+    public float acceleration;
+    public float initVelocity;
+    public float gravity;
+    public Vector2 vGravity; // This will express gravity as a vector which will apply a force downwards
+    public Vector2 vVelocity; // Express velocity as a vector that will change upon incline and declining gradients
 
 	// Use this for initialization
 	void Start () {
-		
+        vGravity = Vector2.down * gravity;
+        vVelocity = Vector2.one * (velocity * gravity);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+       // acceleration = (velocity - initVelocity) / Time.deltaTime;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.left), Velocity * Time.deltaTime);
+            transform.position = ForceMode2D.Force(acceleration) = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.left), velocity * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.right), Velocity * Time.deltaTime);
+            transform.position = Vector2.Lerp(transform.position, transform.TransformPoint(Vector2.right), velocity * Time.deltaTime);
         }
 
     }
